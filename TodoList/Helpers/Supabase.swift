be29ae0cc0eb29d78d1a298ceaeef0a4327f001f
Myers <mmbonda@lcs.on.ca>
2024-05-23 11:21:06 -0400

    added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,14 @@
+//
+//  Supabase.swift
+//  TodoList
+//
+//  Created by Myers Elliott Mbonda on 2024-05-23.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://xawpvtsaurneenspqrpg.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhhd3B2dHNhdXJuZWVuc3BxcnBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyOTg0NDMsImV4cCI6MjAzMTg3NDQ0M30.XUeR-2Eyy8pTzeEDIfYFXtOtUNBZ06GTGEPPERYWNY8"
+)
